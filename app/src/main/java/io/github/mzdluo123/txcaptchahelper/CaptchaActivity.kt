511bd5eda4f85e7f7ec1c644ea0fdae13069fe94
RainChan <mzdluo123@foxmail.com>
2022-05-01 21:34:47 +0800

    fuck

--- app/src/main/java/io/github/mzdluo123/txcaptchahelper/CaptchaActivity.kt
+++ app/src/main/java/io/github/mzdluo123/txcaptchahelper/CaptchaActivity.kt
@@ -1,11 +1,15 @@
 package io.github.mzdluo123.txcaptchahelper
 
+import android.R.attr.x
+import android.R.attr.y
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.graphics.*
 import android.net.Uri
 import android.os.Bundle
+import android.os.SystemClock
 import android.util.Log
+import android.view.MotionEvent
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
@@ -15,6 +19,7 @@ import kotlinx.android.synthetic.main.activity_captcha.*
 import okhttp3.*
 import java.io.IOException
 import java.util.*
+
 
 class CaptchaActivity : AppCompatActivity() {
     companion object {
@@ -59,6 +64,7 @@ class CaptchaActivity : AppCompatActivity() {
             javaScriptEnabled = true
             domStorageEnabled = true
         }
+
     }
 
     private fun onJsBridgeInvoke(request: Uri): Boolean {
@@ -115,7 +121,7 @@ class CaptchaActivity : AppCompatActivity() {
             ) {
                 Log.d(TAG, "height $it")
                 val height = it.toFloat().toInt()
-                val newMap = Bitmap.createBitmap(bitmap, 0, height+10, bitmap.width, 120)
+                val newMap = Bitmap.createBitmap(bitmap, 0, height + 10, bitmap.width, 120)
                 val top2 = calculate(newMap)
                 slide(top2)
             }
@@ -123,6 +129,7 @@ class CaptchaActivity : AppCompatActivity() {
         }
 
     }
+
     fun indexesOfTopElements(orig: IntArray, nummax: Int): IntArray {
         val copy = Arrays.copyOf(orig, orig.size)
         Arrays.sort(copy)
@@ -138,32 +145,88 @@ class CaptchaActivity : AppCompatActivity() {
         return result
     }
 
-    private fun calculate(bitmap:Bitmap): IntArray {
-        val new_map = Bitmap.createBitmap(bitmap.width,bitmap.height,Bitmap.Config.RGB_565)
+    private fun calculate(bitmap: Bitmap): IntArray {
+        val new_map = Bitmap.createBitmap(bitmap.width, bitmap.height, Bitmap.Config.RGB_565)
         val canvas = Canvas(new_map)
         val paint = Paint()
         val cm = ColorMatrix()
         cm.setSaturation(0f)
         paint.colorFilter = ColorMatrixColorFilter(cm)
-        canvas.drawBitmap(bitmap,0f,0f,paint)
+        canvas.drawBitmap(bitmap, 0f, 0f, paint)
         val result = IntArray(bitmap.width) { 0 }
-        for (i in 0 until bitmap.width){
-            for (j in 0 until bitmap.height){
-                val pix = new_map.getPixel(i,j) and 0x000000FF
+        for (i in 0 until bitmap.width) {
+            for (j in 0 until bitmap.height) {
+                val pix = new_map.getPixel(i, j) and 0x000000FF
                 result[i] += pix
             }
         }
-        val top2 = indexesOfTopElements(result,2)
-        val linePaint  =Paint()
+        val top2 = indexesOfTopElements(result, 2)
+        val linePaint = Paint()
         linePaint.color = Color.GREEN
-        Log.i(TAG,top2.toString())
-        canvas.drawLine(top2[0].toFloat(),0f, top2[0].toFloat(),new_map.height.toFloat(),linePaint)
-        canvas.drawLine(top2[1].toFloat(),0f, top2[1].toFloat(),new_map.height.toFloat(),linePaint)
+        canvas.drawLine(
+            top2[0].toFloat(),
+            0f,
+            top2[0].toFloat(),
+            new_map.height.toFloat(),
+            linePaint
+        )
+        canvas.drawLine(
+            top2[1].toFloat(),
+            0f,
+            top2[1].toFloat(),
+            new_map.height.toFloat(),
+            linePaint
+        )
         runOnUiThread {
             img_view.setImageBitmap(new_map)
         }
         return top2
     }
 
-    private fun slide(top2:IntArray){}
+    private fun slide(top2: IntArray) {
+
+        webview.evaluateJavascript("""JSON.stringify( document.getElementById("tcaptcha_drag_thumb").getBoundingClientRect())""") {
+            Log.d(TAG, it)
+            val json = JsonParser.parseString(it.slice(1..it.length - 2).replace("\\", ""))
+            val x = json.asJsonObject.get("x").asFloat
+            val y = json.asJsonObject.get("y").asFloat
+
+            val rand = Random()
+            val totalTime = rand.nextFloat()
+//            webview.dispatchTouchEvent(
+//                MotionEvent.obtain(
+//                    SystemClock.uptimeMillis(),
+//                    SystemClock.uptimeMillis()+1000,
+//                    MotionEvent.ACTION_DOWN,
+//                    x,
+//                    y,
+//                    1f,
+//                    1f,
+//                    0,
+//                    1f,
+//                    1f,
+//                    0,
+//                    0
+//                )
+//            )
+//
+//            webview.dispatchTouchEvent(
+//                MotionEvent.obtain(
+//                    SystemClock.uptimeMillis(),
+//                    SystemClock.uptimeMillis()+1000,
+//                    MotionEvent.ACTION_MOVE,
+//                    x+100,
+//                    y,
+//                    1f,
+//                    1f,
+//                    0,
+//                    1f,
+//                    1f,
+//                    0,
+//                    0
+//                )
+//            )
+
+        }
+    }
 }
